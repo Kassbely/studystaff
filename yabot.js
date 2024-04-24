9b4b56e4d398ce5b989565be27053f229a369e2d
Alexandra Belyakova <164514774+Kassbely@users.noreply.github.com>
2024-04-24 12:37:18 +0300

    Бот  находит целевую страницу, переходит на нее без открытия новой вкладки,  ходит ссылкам на странице

--- yabot.js
+++ yabot.js
@@ -1,53 +1,96 @@
 // ==UserScript==
-// @name Bot yandex
-// @namespace http://tampermonkey.net/
-// @version 1.00
-// @description try to take over the world!
-// @author Belyakova Alexandra
-// @match https://ya.ru/*
-// @grant none
+// @name         yandex bot
+// @namespace    http://tampermonkey.net/
+// @version      1.00
+// @description  Bot for yandex
+// @author       Belyakova Alexandra
+// @match        https://ya.ru/*
+// @match        https://htmlbook.ru/*
+// @grant        none
 // ==/UserScript==
 
-let yaInput = document.getElementById("text");
+let yaInput = document.getElementsByName('text')[0];
 let yaBtn = document.getElementsByClassName("search3__button mini-suggest__button")[0];
 let links = document.links;
-let keywords = ["Хорошие статьи", "Свободная энциклопедия", "Изображение дня"];
+let keywords = ["CSS-анимация для начинающих", "Справочник по HTML"];
 let keyword = keywords[getRandom(0, keywords.length)];
-//работа с главной страницей поиска
+
+
+let mouseClick = new MouseEvent("click");
+
+//Работаем на главной странице
 if (yaBtn !== undefined) {
-  let i = 0;
-  let timerId = setInterval(() => {
-    yaInput.value += keyword[i];
-    i++;
+let i = 0;
+ //  yaInput.focus();
+ //  yaInput.dispatchEvent(mouseClick);
+ //  yaInput.value = keyword;
+ //  yaBtn.click();
+
+   let timerId = setInterval(() => {
+     yaInput.value += keyword[i];
+     i++;
     if (i == keyword.length) {
       clearInterval(timerId);
       yaBtn.click();
     }
-  },500)
-  //Работа с результатами поисковой выдачи
-  } else if (document.getElementsByClassName("HeaderDesktopNavigation-Cutted") !==null) {
-    let nextPage = true;
-    for (let i = 0; i < links.length; i++) {
-      if (links[i].href.indexOf("ru.wikipedia.org") != -1) {
-        let link = links[i];
-        let nextPage = false;
-        console.log("Нашел строку " + link);
-        setTimeout(() => {
-          link.click();
-        }, getRandom(2000, 3000))
-        break;
+   },150)
+  //Работаем на целевом сайте
+  } else if (location.hostname == "htmlbook.ru") {
+
+    setInterval(() => {
+      let linkIndex = getRandom(0, links.length);
+      let localLink = links[linkIndex];
+
+      if (getRandom(0, 101) > 50) {
+        location.href = "https://ya.ru/";
       }
-    }
-    if (document.querySelector('[aria-label="Страница 4"]').innerText == "4") {
+      if (links.length == 0) {
+        location.href = "https://htmlbook.ru/";
+      }
+
+      if (localLink.href.includes("htmlbook.ru")) {
+        localLink.click();
+      }
+    }, getRandom(5000, 7000))
+
+    console.log("Мы на целевом сайте");
+  }
+//Работаем на странице поисковой выдачи
+else if (document.querySelector(".Pager-Content") !== null){
+  let nextPage = true;
+  for (let i = 0; i < links.length; i++) {
+    if (links[i].href.indexOf("htmlbook.ru") != -1) {
+      let link = links[i];
       let nextPage = false;
-      location.href = "https://ya.ru/"
-    }
-    if (nextPage) {
+      console.log("Нашел строку " + link);
       setTimeout(() => {
-        document.querySelector('[aria-label="Следующая страница"]').click();
-      }, getRandom(2500,3500))
+        link.target = '_self';
+        link.click();
+      }, getRandom(2000, 3000));
+      break;
     }
   }
+  let elementExist = setInterval(() => {
+    let elem = document.querySelector('a[aria-label="Первая страница"]');
+    if (elem !== null ) {
+      if (elem.innerText == "5") {
+        let nextPage = false;
+        location.href = "https://ya.ru/";
+      }
+      clearInterval(elementExist);
+    }
+  }, 100)
+
+
+  if (nextPage) {
+    setTimeout(() => {
+      document.querySelector('a[aria-label="Следующая страница"]').click();
+    }, getRandom(2500, 3500))
+
+  }
+}
+
 function getRandom(min,max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
+
