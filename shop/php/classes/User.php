0e659141ac5a7eef0cff39e2333e67231b912860
Alexandra Belyakova <164514774+Kassbely@users.noreply.github.com>
2024-05-14 16:26:36 +0300

    Update User.php

--- shop/php/classes/User.php
+++ shop/php/classes/User.php
@@ -1,9 +1,7 @@
 <?php
 
 class User
-{
-
-  private $name;
+{ private $name;
   private $surname;
   private $email;
   private $id;
@@ -15,26 +13,24 @@ class User
     $this->surname = $surname;
     $this->email = $email;
   }
-  function getId()
-  {
-    return $this->id;
-  }
-  function getName()
-  {
-    return $this->name;
-  }
-  function getSurname()
-  {
-    return $this->surname;
-  }
-  function getEmail()
-  {
-    return $this->email;
-  }
+  function getId() { return $this->id;}
+  function getName() {return $this->name;}
+  function getSurname() {return $this->surname;}
+  function getEmail()  {return $this->email;}
 
   //Статический метод добавления пользователя в бд
-  static function addUser()
+  static function addUser($name, $surname, $email, $pass)
   {
     global $mysqli;
+    $email = mb_strtolower(trim($email));
+    $pass = trim($pass);
+    $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+    $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email'");
+    if ($result->num_rows != 0) {
+      return json_encode(["result" => "exist"]);
+    } else {
+        return json_encode(["result" => "success"]);
+    }
   }
 }
