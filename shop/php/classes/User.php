e523a502cb3437f8518c54b8c3bbf9562e04d952
Alexandra Belyakova <164514774+Kassbely@users.noreply.github.com>
2024-05-14 15:47:09 +0300

    Update User.php

--- shop/php/classes/User.php
+++ shop/php/classes/User.php
@@ -1,31 +1,40 @@
-<?php 
-class User {
-    private $name;
-    private $surname;
-    private $email;
-    private $id;
+<?php
 
-    function __construct($id, $name, $surname, $email) {
-        $this->name = $name;
-        $this->surname = $surname;
-        $this->email = $email;
-        $this->id = $id;
-    }
-    function getId() {
-        return $this->id;
-    }
-    function getName() {
-        return $this->name;
-    }
-    function getSurname() {
-        return $this->surname;
-    }
-    function getEmail() {
-        return $this->email;
-    }
-    //статический метод для добавления пользователя
-    static function addUser() {
-        global $mysqli;
-        var_dump($mysqli);
-    }
+class User
+{
+
+  private $name;
+  private $surname;
+  private $email;
+  private $id;
+
+  function __construct($id, $name, $surname, $email)
+  {
+    $this->id = $id;
+    $this->name = $name;
+    $this->surname = $surname;
+    $this->email = $email;
+  }
+  function getId()
+  {
+    return $this->id;
+  }
+  function getName()
+  {
+    return $this->name;
+  }
+  function getSurname()
+  {
+    return $this->surname;
+  }
+  function getEmail()
+  {
+    return $this->email;
+  }
+
+  //Статический метод добавления пользователя в бд
+  static function addUser()
+  {
+    global $mysqli;
+  }
 }
